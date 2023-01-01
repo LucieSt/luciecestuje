e36889eb71fa0316b75ae2f6ace26244d2b45ae3
LucieSt <strakova.91@gmail.com>
2023-01-01 21:42:24 +0100

    planning

--- src/Travels/index.js
+++ src/Travels/index.js
@@ -6,8 +6,8 @@ export const Travels = () => {
         <p>Tohle je stranka s cestami
         <br/>bude tu vycet cest podle let (ctverce) nebo proste siroke pasy jako minule a nejaky filtracni system - roky, zeme
         <br/>asi bude nejlepsi nechat to jak bylo - radit od nejaktualnejsich cest a nahoru dat filtrovat podle ...
-        <br/>tady bude appendovana komponenta Seznam cest a bude mozne se prokliknout do konkretni cesty (komponenta Cesta)
-        <br/><br/>databaze - bude obsahovat seznam cest, ale zaroven i obrazky a texty jednotlivych cest ktere se z ni budou nacitat?</p>
+        <br/>tady bude appendovana komponenta ListOfTravels a bude mozne se prokliknout do konkretni cesty (komponenta Travel)
+        <br/><br/>databaze - bude obsahovat ListOfTravels, ale zaroven i obrazky a texty jednotlivych cest ktere se z ni budou nacitat?</p>
     `
 
     return element;
