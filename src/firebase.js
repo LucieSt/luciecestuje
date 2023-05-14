31f95fd284d6ae9a65763cde566558d3fea5481d
Lucie Boušková <strakova.91@gmail.com>
2023-05-14 19:37:43 +0200

    setup of firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCWPxvGgVRJ5oSx71jOylIFtcL86ZxciQA",
-  authDomain: "luciecestuje.firebaseapp.com",
-  projectId: "luciecestuje",
-  storageBucket: "luciecestuje.appspot.com",
-  messagingSenderId: "237005623966",
-  appId: "1:237005623966:web:a95a308bd17bb3eb7ca18f",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 
