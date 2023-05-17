d1a163bd36f12306932238145fdb4c653a07613c
Lucie Boušková <strakova.91@gmail.com>
2023-05-17 20:42:27 +0200

    data taken from firebase

--- src/components/travel.jsx
+++ src/components/travel.jsx
@@ -1,53 +1,39 @@
 import { useParams } from "react-router-dom";
-
-const cesty = [
-  {
-    id: 1,
-    title: "Vietnam",
-    text: "bla bla bla",
-    year: 2019,
-    images: ["as56", "344hh", "444h"],
-  },
-  {
-    id: 2,
-    title: "Toskansko",
-    text: "bla bluuu bla",
-    year: 2022,
-    images: ["555l", "344hh", "333l"],
-  },
-  {
-    id: 3,
-    title: "Madeira",
-    text: "bloo bluu",
-    year: 2022,
-    images: ["ddd3", "33g", "444h"],
-  },
-  {
-    id: 4,
-    title: "Barcelona",
-    text: "blii blaaa bluu",
-    year: 2022,
-    images: ["as56", "33g", "444h"],
-  },
-  {
-    id: 5,
-    title: "Fuerteventura a Tenerife",
-    text: "bla blu blaaaai",
-    year: 2023,
-    images: ["555l", "344hh", "333l"],
-  },
-];
+import { useState, useEffect } from "react";
+import { db } from "./../firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 const Travel = () => {
+  const [selectedTravelData, setSelectedTravelData] = useState(null);
+
   const { travelId } = useParams();
-  const travelData = cesty.find((cesta) => cesta.id.toString() === travelId);
-  const { title, text, year } = travelData;
+
+  const displayData = async () => {
+    const querySnapshot = await getDocs(collection(db, "travels"));
+    const newData = querySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+
+    const foundTravelData = newData.find((cesta) => cesta.title === travelId);
+    setSelectedTravelData(foundTravelData);
+  };
+
+  useEffect(() => {
+    displayData();
+  }, []);
+
+  const { title, year, text } = selectedTravelData || {};
 
   return (
     <div className="travel">
-      <h3>{title}</h3>
-      <h4>{year}</h4>
-      <p>{text}</p>
+      {selectedTravelData && (
+        <>
+          <h3>{title}</h3>
+          <h4>{year}</h4>
+          <p>{text}</p>
+        </>
+      )}
     </div>
   );
 };
--- src/components/travels.jsx
+++ src/components/travels.jsx
@@ -1,57 +1,40 @@
 import "./../styles/travels.sass";
 import { Link } from "react-router-dom";
-
-const cesty = [
-  {
-    id: 1,
-    title: "Vietnam",
-    text: "bla bla bla",
-    year: 2019,
-    images: ["as56", "344hh", "444h"],
-  },
-  {
-    id: 2,
-    title: "Toskansko",
-    text: "bla bluuu bla",
-    year: 2022,
-    images: ["555l", "344hh", "333l"],
-  },
-  {
-    id: 3,
-    title: "Madeira",
-    text: "bloo bluu",
-    year: 2022,
-    images: ["ddd3", "33g", "444h"],
-  },
-  {
-    id: 4,
-    title: "Barcelona",
-    text: "blii blaaa bluu",
-    year: 2022,
-    images: ["as56", "33g", "444h"],
-  },
-  {
-    id: 5,
-    title: "Fuerteventura a Tenerife",
-    text: "bla blu blaaaai",
-    year: 2023,
-    images: ["555l", "344hh", "333l"],
-  },
-];
+import { useState, useEffect } from "react";
+import { db } from "./../firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 const Travels = () => {
+  const [travelData, setTravelData] = useState([]);
+
+  const displayData = async () => {
+    await getDocs(collection(db, "travels")).then((querySnapshot) => {
+      const newData = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setTravelData(newData);
+      console.log(travelData, newData);
+    });
+  };
+
+  useEffect(() => {
+    displayData();
+  }, []);
+
   return (
     <div className="travels">
       <h2>cesty</h2>
       <div className="travels-list">
-        {cesty.map((cesta) => {
-          const url = `/cesty/${cesta.id}`;
-          return (
-            <Link key={cesta.id} to={url}>
-              {cesta.title}
-            </Link>
-          );
-        })}
+        {travelData &&
+          travelData.map((cesta) => {
+            const url = `/cesty/${cesta.title}`;
+            return (
+              <Link key={cesta.id} to={url}>
+                {cesta.title}
+              </Link>
+            );
+          })}
       </div>
     </div>
   );
