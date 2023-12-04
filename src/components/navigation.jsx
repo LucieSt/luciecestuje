4bad9cb4e52bb4b376fad529862dfc74fe955f40
LucieSt <strakova.91@gmail.com>
2023-12-04 20:16:39 +0100

    navigation changed

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./../styles/navigation.sass";
 import { AuthContext } from "../authContext";
@@ -8,6 +8,13 @@ import { signOut } from "firebase/auth";
 const Navigation = () => {
   const { signedIn } = useContext(AuthContext);
   const navigate = useNavigate();
+
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 30);
+    });
+  });
 
   const handleLogout = () => {
     const auth = getAuth();
@@ -23,23 +30,27 @@ const Navigation = () => {
   };
 
   return (
-    <div className="nav-container">
-      <span className="nav-logo">
-        <Link to="/">luciecestuje</Link>
-      </span>
-      <nav className="nav-links">
-        <Link to="/cesty" className="nav-travels">
-          cesty
-        </Link>
-        <Link to="/onas" className="nav-about">
-          o nas
-        </Link>
-        {signedIn && (
-          <span className="nav-signout" onClick={handleLogout}>
-            odhlasit
+    <div className="nav-section">
+      <header className={`nav-wrapper ${scroll ? "sticky" : ""}`}>
+        <div className="nav-container">
+          <span className="nav-logo">
+            <Link to="/">luciecestuje</Link>
           </span>
-        )}
-      </nav>
+          <nav className="nav-links">
+            <Link to="/cesty" className="nav-travels">
+              cesty
+            </Link>
+            <Link to="/onas" className="nav-about">
+              o nas
+            </Link>
+            {signedIn && (
+              <span className="nav-signout" onClick={handleLogout}>
+                odhlasit
+              </span>
+            )}
+          </nav>
+        </div>
+      </header>
     </div>
   );
 };
