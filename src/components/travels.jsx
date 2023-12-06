293b75d6a415eabb22218f9203ae96c736f5002b
LucieSt <strakova.91@gmail.com>
2023-12-06 20:37:49 +0100

    travels component altered

--- src/components/travels.jsx
+++ src/components/travels.jsx
@@ -27,45 +27,48 @@ const Travels = () => {
     <div className="travels">
       <div className="travels-banner banner"></div>
 
-      <p>blabla fhbjdsjh<br />bsdjsdfh sf <br />hjksfghdfndf   fdf gfh jkfh sfj k f<br />sjh fkj  dgdgg df d gdfgsfbksfb f<br />bfsd bfsfb<br />s bfsd f</p>
+      <div className="content">
+        <p>blabla fhbjdsjh<br />bsdjsdfh sf <br />hjksfghdfndf   fdf gfh jkfh sfj k f<br />sjh fkj  dgdgg df d gdfgsfbksfb f<br />bfsd bfsfb<br />s bfsd f</p>
 
-      <div className="travels-list">
-        {signedIn && (
-          <Link to="/novacesta">
-            <div className="travels-link-container">
-              <div className="travels-img">
-                <img
-                  src="https://res.cloudinary.com/dnwbnhdof/image/upload/c_scale,w_auto/dpr_auto/v1684698763/xhyozoujfrrnzisptc6v.jpg"
-                  alt="new"
-                  width="100%"
-                  height="auto"
-                />
-                <h3>+</h3>
-              </div>
-            </div>
-          </Link>
-        )}
-        {travelData &&
-          travelData.map((cesta) => {
-            console.log(cesta);
-            const url = `/cesty/${cesta.title}`;
-            return (
-              <Link key={cesta.id} to={url}>
-                <div className="travels-link-container">
-                  <div className="travels-img">
-                    <img
-                      src={cesta.main_image}
-                      alt={cesta.title}
-                      width="100%"
-                      height="auto"
-                    />
-                    <h3>{cesta.title}</h3>
-                  </div>
+        <div className="travels-list">
+          {signedIn && (
+            <Link to="/novacesta">
+              <div className="travels-link-container">
+                <div className="travels-img">
+                  <img
+                    src="https://res.cloudinary.com/dnwbnhdof/image/upload/c_scale,w_auto/dpr_auto/v1684698763/xhyozoujfrrnzisptc6v.jpg"
+                    alt="new"
+                    width="100%"
+                    height="auto"
+                  />
+                  <h3>+</h3>
                 </div>
-              </Link>
-            );
-          })}
+              </div>
+            </Link>
+          )}
+          {travelData &&
+            travelData.map((cesta) => {
+              console.log(cesta);
+              const url = `/cesty/${cesta.title}`;
+              return (
+                <Link key={cesta.id} to={url}>
+                  <div className="travels-link-container">
+                    <div className="travels-img">
+                      <img
+                        src={cesta.main_image}
+                        alt={cesta.title}
+                        width="100%"
+                        height="auto"
+                      />
+                      <h3>{cesta.title}</h3>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
+        </div>
       </div>
+
     </div>
   );
 };
